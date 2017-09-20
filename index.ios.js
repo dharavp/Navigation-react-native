e43778ff5eee54742799f96f1a2da2b85b3c4137
dharavp <pateldhara248@gmail.com>
2017-09-20 12:47:01 +0530

    add nav drawer screen in index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -10,7 +10,9 @@ import HomeScreen from './src/HomeScreen';
 import SecondScreen from './src/SecondScreen';
 import FirstNavWithArgs from './src/NavWithArgs/FirstNavWithArgs';
 import SecondNavWithArgs from './src/NavWithArgs/SecondNavWithArgs';
-import TabNavigationFirstScreen from './src/TabNavigation/TabNavigationFirstScreen';
+import TabNavigatorWrappingScreen from './src/TabNavigation/TabNavigatorWrappingScreen';
+import MaterialTabNav from './src/TabNavigation/MaterialTabNav';
+import App from './src/NavDraw/NavDrawWrapping';
 
  const SimpleApp = StackNavigator({
   App: { screen: app },
@@ -18,7 +20,9 @@ import TabNavigationFirstScreen from './src/TabNavigation/TabNavigationFirstScre
   SecondScreen: { screen: SecondScreen },
   ArgsFirstScreen: { screen: FirstNavWithArgs },
   ArgsSecondScreen: { screen: SecondNavWithArgs },
-  TabNavigation: { screen: TabNavigationFirstScreen }
+  TabNavigation: { screen: TabNavigatorWrappingScreen, navigationOptions: { title: 'Tab Navigation' } },
+  MaterialTabNav: { screen: MaterialTabNav, navigationOptions: { title: 'Material Tab Navigation' } },
+  NavDraw: { screen: App }
 }
 );
 
